6bd92e39db61b4cb2e51792404c5b40602975819
Viren Mody <virenmody@gmail.com>
2018-11-06 16:31:38 -0600

    Added content authority and uri to contract

--- app/src/main/java/com/example/android/inventoryappstage1/data/InventoryContract.java
+++ app/src/main/java/com/example/android/inventoryappstage1/data/InventoryContract.java
@@ -1,15 +1,23 @@
 package com.example.android.inventoryappstage1.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class InventoryContract {
 
     private InventoryContract() {}
 
+    public static final String CONTENT_AUTHORITY = "com.example.android.inventoryappstage1";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_INVENTORY = "inventory";
+
     /**
      * Each database table needs its own inner class - this one is for store inventory
      */
     public static final class InventoryEntry implements BaseColumns {
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,
+                PATH_INVENTORY);
 
         public final static String TABLE_NAME = "inventory";
         public final static String _ID = BaseColumns._ID;
